b91826d9878d66bcb7c955edae04142cd37703d2
trevordarst <trevordarst@gmail.com>
2024-02-11 22:43:15 -0500

    Got rid of another uneeded validlib function and added documentation to validlib

--- LC3/Scripts/Supporting_Libraries/validlib.py
+++ LC3/Scripts/Supporting_Libraries/validlib.py
@@ -9,9 +9,8 @@ ERROR_RETURN_TO_ASSEMBLER = lambda line, line_contents, error: f"*** ERROR <line
 ERROR_OPCODE_INVALID_OPCODE = lambda given_op: f"Opcode {given_op} does not exist"
 
 ERROR_OPERAND_TYPE_STR = lambda op: f"Invalid operand {op}"
-ERROR_OPERAND_LENGTH_STR = lambda req_len, op_len : f"Should be {req_len} operands, but {op_len} given"
+ERROR_OPERAND_LENGTH_STR = lambda req_len, op_len : f"Requires {req_len} operands, but {op_len} given"
 ERROR_OPERAND_INVALID_LABEL = lambda label: f"Label {label} is invalid"
-
 ERROR_OPERAND_INVALID_TRAP_VECTOR = lambda vector: f"Trap vector {vector} is invalid"
 ERROR_OPERAND_VALUE_OUT_OF_RANGE = lambda value, range: f"Value {value} not in {range}"
 ERROR_OPERAND_INVALID_STRING_NOT_ENCLOSED = "Invalid string given"
@@ -19,7 +18,8 @@ ERROR_OPERAND_INVALID_STRING_LENGTH = lambda length, range: f"String length {len
 
 # ===============================================================================
 # Name: validate_add_and
-# Purpose: Determines whether line is a valid use of ADD or AND.
+# Purpose: Determines whether line is a valid use of ADD or AND. Checks for
+#          individual operand errors as well as number of operands
 # ===============================================================================
 def valid_add_and(tokens: dict, label_lookup: dict) -> str:
     error_str = ''
@@ -46,7 +46,11 @@ def valid_add_and(tokens: dict, label_lookup: dict) -> str:
 
     return error_str
     
-
+# ===============================================================================
+# Name: valid_jmp_jsrr
+# Purpose: Determines whether line is a valid use of JMP or JSRR. Checks for
+#          individual operand errors as well as number of operands
+# ===============================================================================
 def valid_jmp_jsrr(tokens: dict, label_lookup: dict) -> str:
     error_str = ''
     operands = tokens[KEY_OPERANDS]
@@ -64,6 +68,11 @@ def valid_jmp_jsrr(tokens: dict, label_lookup: dict) -> str:
 
     return error_str
 
+# ===============================================================================
+# Name: valid_ld_ldi_lea
+# Purpose: Determines whether line is a valid use of LD or LADI OR LEA. Checks for
+#          individual operand errors as well as number of operands
+# ===============================================================================
 def valid_ld_ldi_lea(tokens: dict, label_lookup: dict) -> str:
     error_str = ''
     operands = tokens[KEY_OPERANDS]
@@ -85,6 +94,11 @@ def valid_ld_ldi_lea(tokens: dict, label_lookup: dict) -> str:
 
     return error_str
 
+# ===============================================================================
+# Name: valid_ldr_str
+# Purpose: Determines whether line is a valid use of LDR OR STR. Checks for
+#          individual operand errors as well as number of operands
+# ===============================================================================
 def valid_ldr_str(tokens: dict, label_lookup: dict) -> str:
     error_str = ''
     operands = tokens[KEY_OPERANDS]
@@ -110,6 +124,11 @@ def valid_ldr_str(tokens: dict, label_lookup: dict) -> str:
 
     return error_str
 
+# ===============================================================================
+# Name: valid_st_sti
+# Purpose: Determines whether line is a valid use of ST OR STI. Checks for
+#          individual operand errors as well as number of operands
+# ===============================================================================
 def valid_st_sti(tokens: dict, label_lookup: dict) -> str:
     error_str = ''
     operands = tokens[KEY_OPERANDS]
@@ -131,6 +150,11 @@ def valid_st_sti(tokens: dict, label_lookup: dict) -> str:
 
     return error_str
 
+# ================================================================================
+# Name: valid_ret_rti_end_explicit_trap
+# Purpose: Determines whether line is a valid use of RET, RTI, or an explicit trap
+#          call. Checks forindividual operand errors as well as number of operands
+# ================================================================================
 def valid_ret_rti_end_explicit_trap(tokens: dict, label_lookup: dict) -> str:
     error_str = ''
     operands = tokens[KEY_OPERANDS]
@@ -140,6 +164,11 @@ def valid_ret_rti_end_explicit_trap(tokens: dict, label_lookup: dict) -> str:
     
     return error_str
 
+# ===============================================================================
+# Name: valid_jsr_br
+# Purpose: Determines whether line is a valid use of JSR OR BR. Checks for
+#          individual operand errors as well as number of operands
+# ===============================================================================
 def valid_jsr_br(tokens: dict, label_lookup: dict) -> str:
     error_str = ''
     operands = tokens[KEY_OPERANDS]
@@ -157,6 +186,11 @@ def valid_jsr_br(tokens: dict, label_lookup: dict) -> str:
 
     return error_str
 
+# ===============================================================================
+# Name: valid_not
+# Purpose: Determines whether line is a valid use of NOT. Checks for
+#          individual operand errors as well as number of operands
+# ===============================================================================
 def valid_not(tokens: dict, label_lookup: dict) -> str:
     error_str = ''
     operands = tokens[KEY_OPERANDS]
@@ -178,6 +212,11 @@ def valid_not(tokens: dict, label_lookup: dict) -> str:
 
     return error_str 
 
+# ===============================================================================
+# Name: valid_trap
+# Purpose: Determines whether line is a valid use of TRAP. Checks for
+#          individual operand errors as well as number of operands
+# ===============================================================================
 def valid_trap(tokens: dict, label_lookup: dict) -> str:
     error_str = ''
     operands = tokens[KEY_OPERANDS]
@@ -195,6 +234,11 @@ def valid_trap(tokens: dict, label_lookup: dict) -> str:
 
     return error_str
 
+# ===============================================================================
+# Name: valid_orig
+# Purpose: Determines whether line is a valid use of ORIG. Checks for
+#          individual operand errors as well as number of operands
+# ===============================================================================
 def valid_orig(tokens: dict, label_lookup: dict) -> str:
     error_str = ''
     operands = tokens[KEY_OPERANDS]
@@ -212,6 +256,11 @@ def valid_orig(tokens: dict, label_lookup: dict) -> str:
 
     return error_str
 
+# ===============================================================================
+# Name: valid_fill
+# Purpose: Determines whether line is a valid use of FILL. Checks for
+#          individual operand errors as well as number of operands
+# ===============================================================================
 def valid_fill(tokens: dict, label_lookup: dict) -> str:
     error_str = ''
     operands = tokens[KEY_OPERANDS]
@@ -238,6 +287,11 @@ def valid_fill(tokens: dict, label_lookup: dict) -> str:
 
     return error_str
 
+# ===============================================================================
+# Name: valid_blkw
+# Purpose: Determines whether line is a valid use of BLKW. Checks for
+#          individual operand errors as well as number of operands
+# ===============================================================================
 def valid_blkw(tokens: dict, label_lookup: dict) -> str:
     error_str = ''
     operands = tokens[KEY_OPERANDS]
@@ -264,6 +318,11 @@ def valid_blkw(tokens: dict, label_lookup: dict) -> str:
 
     return error_str
 
+# ===============================================================================
+# Name: valid_stringz
+# Purpose: Determines whether line is a valid use of STRINGZ. Checks for
+#          individual operand errors as well as number of operands
+# ===============================================================================
 def valid_stringz(tokens: dict, label_lookup: dict) -> str:
     error_str = ''
     operands = tokens[KEY_OPERANDS]
@@ -281,54 +340,126 @@ def valid_stringz(tokens: dict, label_lookup: dict) -> str:
 
     return error_str
 
+# ===============================================================================
+# Name: valid_add
+# Purpose: Uses combined validation function valid_add_and to validate.
+# ===============================================================================
 def valid_add(tokens: dict, label_lookup: dict) -> str:
     return valid_add_and(tokens, label_lookup)
 
+# ===============================================================================
+# Name: valid_and
+# Purpose: Uses combined validation function valid_add_and to validate.
+# ===============================================================================
 def valid_and(tokens: dict, label_lookup: dict) -> str:
     return valid_add_and(tokens, label_lookup)
 
+# ===============================================================================
+# Name: valid_jmp
+# Purpose: Uses combined validation function valid_jmp_jsrr to validate.
+# ===============================================================================
 def valid_jmp(tokens: dict, label_lookup: dict) -> str:
     return valid_jmp_jsrr(tokens, label_lookup)
 
+# ===============================================================================
+# Name: valid_jsrr
+# Purpose: Uses combined validation function valid_jmp_jsrr to validate.
+# ===============================================================================
 def valid_jsrr(tokens: dict, label_lookup: dict) -> str:
     return valid_jmp_jsrr(tokens, label_lookup)
 
+# ===============================================================================
+# Name: valid_jsr
+# Purpose: Uses combined validation function valid_jsr_br to validate.
+# ===============================================================================
 def valid_jsr(tokens: dict, label_lookup: dict) -> str:
     return valid_jsr_br(tokens, label_lookup)
 
+# ===============================================================================
+# Name: valid_br
+# Purpose: Uses combined validation function valid_jsr_br to validate.
+# ===============================================================================
 def valid_br(tokens: dict, label_lookup: dict) -> str:
     return valid_jsr_br(tokens, label_lookup)
 
+# ===============================================================================
+# Name: valid_ld
+# Purpose: Uses combined validation function valid_ld_ldi_lea to validate.
+# ===============================================================================
 def valid_ld(tokens: dict, label_lookup: dict) -> str:
     return valid_ld_ldi_lea(tokens, label_lookup)
 
+# ===============================================================================
+# Name: valid_ldi
+# Purpose: Uses combined validation function valid_ld_ldi_lea to validate.
+# ===============================================================================
 def valid_ldi(tokens: dict, label_lookup: dict) -> str:
     return valid_ld_ldi_lea(tokens, label_lookup)
 
+# ===============================================================================
+# Name: valid_ldr
+# Purpose: Uses combined validation function valid_ldr_str to validate.
+# ===============================================================================
 def valid_ldr(tokens: dict, label_lookup: dict) -> str:
     return valid_ldr_str(tokens, label_lookup)
 
+# ===============================================================================
+# Name: valid_str
+# Purpose: Uses combined validation function valid_ldr_str to validate.
+# ===============================================================================
 def valid_str(tokens: dict, label_lookup: dict) -> str:
     return valid_ldr_str(tokens, label_lookup)
 
+# ===============================================================================
+# Name: valid_lea
+# Purpose: Uses combined validation function valid_ld_ldi_lea to validate.
+# ===============================================================================
 def valid_lea(tokens: dict, label_lookup: dict) -> str:
     return valid_ld_ldi_lea(tokens, label_lookup)
 
+# ===============================================================================
+# Name: valid_ret
+# Purpose: Uses combined validation function valid_ret_rti_end_explicit_trap to 
+#          validate.
+# ===============================================================================
 def valid_ret(tokens: dict, label_lookup: dict) -> str:
     return valid_ret_rti_end_explicit_trap(tokens, label_lookup)
 
+# ===============================================================================
+# Name: valid_rti
+# Purpose: Uses combined validation function valid_ret_rti_end_explicit_trap to 
+#          validate.
+# ===============================================================================
 def valid_rti(tokens: dict, label_lookup: dict) -> str:
     return valid_ret_rti_end_explicit_trap(tokens, label_lookup)
 
+# ===============================================================================
+# Name: valid_explicit_trap
+# Purpose: Uses combined validation function valid_ret_rti_end_explicit_trap to 
+#          validate.
+# ===============================================================================
 def valid_explicit_trap(tokens: dict, label_lookup: dict) -> str:
     return valid_ret_rti_end_explicit_trap(tokens, label_lookup)
 
+# ===============================================================================
+# Name: valid_end
+# Purpose: Uses combined validation function valid_ret_rti_end_explicit_trap to 
+#          validate.
+# ===============================================================================
 def valid_end(tokens: dict, label_lookup: dict) -> str:
     return valid_ret_rti_end_explicit_trap(tokens, label_lookup)
 
+# ===============================================================================
+# Name: valid_st
+# Purpose: Uses combined validation function valid_st_sti to validate.
+# ===============================================================================
 def valid_st(tokens: dict, label_lookup: dict) -> str:
     return valid_st_sti(tokens, label_lookup)
 
+# ===============================================================================
+# Name: valid_sti
+# Purpose: Uses combined validation function valid_st_sti to validate.
+# ===============================================================================
 def valid_sti(tokens: dict, label_lookup: dict) -> str:
     return valid_st_sti(tokens, label_lookup)
     
@@ -382,9 +513,10 @@ VALID_DICT = {
     '.STRINGZ': valid_stringz
 }
 
-def invalid_opcode(opcode: str) -> bool:
-    return not opcode in VALID_DICT.keys()
-
+# ===============================================================================
+# Name: assemble_line_contents
+# Purpose: Uses tokens to assemble the original written line for error output.
+# ===============================================================================
 def assemble_line_contents(tokens: dict) -> str:
     label = tokens[KEY_LABELS]
     opcode = tokens[KEY_OPCODE]
@@ -392,10 +524,15 @@ def assemble_line_contents(tokens: dict) -> str:
     line_contents = f"{' '.join(label) if label else ''} {opcode} {' '.join(operands) if operands else ''}"
     return line_contents
 
-
+# ===============================================================================
+# Name: validate_line
+# Purpose: Checks if there is an opcode error, if not checks if there's an
+#          operand error, otherwise returns empty string.
+# ===============================================================================
 def validate_line(line: int, tokens: dict, label_lookup: dict) -> str:
     error_str = ''
     line_contents = assemble_line_contents(tokens)
+    opcode = tokens[KEY_OPCODE]
 
     try:
         opcode = tokens[KEY_OPCODE]
